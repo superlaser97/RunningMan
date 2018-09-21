4913a4e0f8483fdd1c4933634f1b9dd1c2be1e99
Low Zhi Heng <lowzhiheng@gmail.com>
2018-09-21 18:23:26 +0800

    Initial Commit

---
+++ Assets/GameUIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    [Header("KeyboardControls")]
+    [SerializeField]
+    private bool useKeyboardControls = false;
+    [SerializeField]
+    private KeyCode leftMovement = KeyCode.Minus;
+    [SerializeField]
+    private KeyCode rightMovement = KeyCode.Equals;
+
+    [Header("UI Elements")]
+    [SerializeField]
+    private Image movementSpdBar;
+
+    private float targetMovementSpd;
+
+    private void Update()
+    {
+        if(useKeyboardControls)
+        {
+            if(Input.GetKeyDown(KeyCode.Minus) && PlayerController.instance)
+                PlayerController.instance.OnMovementBtnClick(PlayerController.MovementDirection.LEFT);
+
+            if (Input.GetKeyDown(KeyCode.Equals) && PlayerController.instance)
+                PlayerController.instance.OnMovementBtnClick(PlayerController.MovementDirection.RIGHT);
+        }
+
+        UpdateMovementSpdBar();
+    }
+
+    private void UpdateMovementSpdBar()
+    {
+        if (movementSpdBar && PlayerController.instance)
+            targetMovementSpd = PlayerController.instance.GetCurrentSpeed();
+
+        movementSpdBar.fillAmount = Mathf.Lerp(movementSpdBar.fillAmount, targetMovementSpd, 10 * Time.deltaTime);
+    }
+
+    public void LeftMovementBtnClick()
+    {
+        if (PlayerController.instance)
+            PlayerController.instance.OnMovementBtnClick(PlayerController.MovementDirection.LEFT);
+    }
+
+    public void RightMovementBtnClick()
+    {
+        if (PlayerController.instance)
+            PlayerController.instance.OnMovementBtnClick(PlayerController.MovementDirection.RIGHT);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    private Rigidbody2D m_RigidBody2D;
+
+    [Header("Movement Parameters")]
+    [SerializeField]
+    private float maxMoveSpd = 200f;
+    [SerializeField]
+    private float movementSpdIncreaseAmt = 0.1f;
+    [SerializeField]
+    private float movementSpdDecay = 0.5f;
+    [SerializeField]
+    private float minTapInterval = 0.3f;
+    [SerializeField]
+    private float jumpTapMaxDelay = 0.1f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField]
+    private float jumpForce = 5f;
+    
+    private float currMovementSpd;
+    private float lastTapDuration = 0f;
+    public enum MovementDirection { NEUTRAL, LEFT, RIGHT };
+    private MovementDirection currMovementDir = MovementDirection.NEUTRAL;
+
+    private void Start()
+    {
+        if(instance)
+        {
+            Debug.Log("Player Controller already exists, removing duplicate");
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMovementBtnClick(MovementDirection dir)
+    {
+        if (dir != currMovementDir)
+        {
+            if(lastTapDuration < jumpTapMaxDelay)
+            {
+                Jump();
+                return;
+            }
+
+            currMovementSpd += movementSpdIncreaseAmt;
+            if (currMovementSpd > 1)
+                currMovementSpd = 1;
+        }
+        else
+        {
+            currMovementSpd = 0;
+            //Trip
+        }
+        lastTapDuration = 0;
+        currMovementDir = dir;
+    }
+
+    private void Update()
+    {
+        lastTapDuration += Time.deltaTime;
+        currMovementSpd -= movementSpdDecay * Time.deltaTime;
+
+        if (currMovementSpd < 0 || lastTapDuration > minTapInterval)
+            currMovementSpd = 0;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        m_RigidBody2D.velocity = new Vector2(currMovementSpd * Time.deltaTime * maxMoveSpd, m_RigidBody2D.velocity.y);
+    }
+
+    [ContextMenu("Jump")]
+    private void Jump()
+    {
+        m_RigidBody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public float GetCurrentSpeed()
+    {
+        return currMovementSpd;
+    }
+}
